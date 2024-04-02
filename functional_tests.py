25d99479ee71b1a1d02963f732941951edd6cfce
Andrey Makarov <i@makarov-podolsk.ru>
2024-04-02 08:04:48 +0300

    Добавлен функциональный тест на unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,28 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Congratulations' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    'Тест нового посетителя'
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_event_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('Organizer', self.browser.title)
+        self.fail('Закончить тест!')
+
+        # Предлагается ввести событие
+        # Набираем в текстовом поле "Тестовое событие 1" 
+        # Набираем в текстовом поле "Тестовое событие 2"
+        # Выводится URL
+        # Посещаем URL и видим свои два тестовых события
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
