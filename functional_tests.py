3a0efd1347a7557ca6c1791f4e6287b0c4c98994
Andrey Makarov <i@makarov-podolsk.ru>
2024-04-03 17:55:02 +0300

    ФТ теперь проверяет возможность создать событие

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,12 +16,31 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_event_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
+        # Пользователь видит заголовок и шапку сайта
         self.assertIn('Organizer', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element(by='tag name', value='h1')
+        self.assertIn('Organizer', header_text)
 
         # Предлагается ввести событие
+        inputbox = self.browser.find_element(value='id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Новое событие'
+        )
         # Набираем в текстовом поле "Тестовое событие 1" 
+        inputbox.send_keys('Тестовое событие 1')
+        # После нажатия Enter, страница обновляется и содержит 
+        # "1: Тестовое событие 1"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(value='id_events_table')
+        rows = table.find_elements(by='tag name', value='tr')
+        self.assertTrue(
+            any(row.text == '1: Тестовое событие 1' for row in rows)
+        )
         # Набираем в текстовом поле "Тестовое событие 2"
+        self.fail('Закончить тест!')
         # Выводится URL
         # Посещаем URL и видим свои два тестовых события
 
