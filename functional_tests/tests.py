33e673c3f943937eaa8f1bbd7b7198f16db9841f
Andrey Makarov <i@makarov-podolsk.ru>
2024-04-06 21:55:00 +0300

    В ФТ введена проверка многопользовательности

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 import time
 
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
     'Тест нового посетителя'
@@ -13,13 +16,21 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        tables = self.browser.find_elements(by='tag name', value='table')
-        table = [t for t in tables if t.get_attribute('id') == 'id_events_table'][0]
-        rows = table.find_elements(by='tag name', value='tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
-    def test_can_start_event_list_and_retrieve_it_later(self):
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                tables = self.browser.find_elements(by='tag name', value='table')
+                table = [t for t in tables if t.get_attribute('id') == 'id_events_table'][0]
+                rows = table.find_elements(by='tag name', value='tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+        
+    def test_can_start_a_list_for_one_user(self):
         self.browser.get(self.live_server_url)
 
         # Пользователь видит заголовок и шапку сайта
@@ -40,8 +51,7 @@ class NewVisitorTest(LiveServerTestCase):
         # После нажатия Enter, страница обновляется и содержит 
         # "1: Тестовое событие 1"
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
-        self.check_for_row_in_list_table('1: Тестовое событие 1')
+        self.wait_for_row_in_list_table('1: Тестовое событие 1')
         
         # Набираем в текстовом поле "Тестовое событие 2"
         # Предлагается ввести событие
@@ -51,12 +61,38 @@ class NewVisitorTest(LiveServerTestCase):
         # После нажатия Enter, страница обновляется и содержит 
         # "2: Тестовое событие 2"
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
-        self.check_for_row_in_list_table('1: Тестовое событие 1')
-        self.check_for_row_in_list_table('2: Тестовое событие 2')
+        self.wait_for_row_in_list_table('1: Тестовое событие 1')
+        self.wait_for_row_in_list_table('2: Тестовое событие 2')
 
-        # Выводится URL с сохраненными событиями
-        self.fail('Закончить тест!')
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        input_boxes = self.browser.find_elements(by='tag name', value='input')
+        inputbox = [ib for ib in input_boxes if ib.get_attribute('id') == 'id_new_item'][0]
+        inputbox.send_keys('Тестовое событие 1')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Тестовое событие 1')
 
-        # Посещаем URL и видим свои два тестовых события
+        user1_list_url = self.browser.current_url
+        self.assertRegex(user1_list_url, '/events/.+')
 
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(by='tag name', value='body').text
+        self.assertNotIn('Тестовое событие 1', page_text)
+        self.assertNotIn('Тестовое событие 2', page_text)
+
+        input_boxes = self.browser.find_elements(by='tag name', value='input')
+        inputbox = [ib for ib in input_boxes if ib.get_attribute('id') == 'id_new_item'][0]
+        inputbox.send_keys('Тестовое событие 3')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Тестовое событие 3')
+
+        user2_list_url = self.browser.current_url
+        self.assertRegex(user2_list_url, '/events/.+')
+        self.assertNotEqual(user2_list_url, user1_list_url)
+
+        page_text = self.browser.find_element(by='tag name', value='body').text
+        self.assertNotIn('Тестовое событие 1', page_text)
+        self.assertIn('Тестовое событие 3', page_text)
