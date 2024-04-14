d112f1c507b6447c30d1040787f8e61123af7641
Makarov Andrey Vladimirovich <mkr@development.makarov-podolsk.ru>
2024-04-14 18:19:39 +0300

    Новый URL + представление для добавления в существующие списки. ФТ проходит

--- events/tests.py
+++ events/tests.py
@@ -39,18 +39,21 @@ class ListAndItemModelTest(TestCase):
 class EventsViewTest(TestCase):
 
     def test_uses_list_template(self):
-        response = self.client.get('/events/list1/')
+        list_ = List.objects.create()
+        response = self.client.get(f'/events/{list_.id}/')
         self.assertTemplateUsed(response, 'list.html')
     
-    def test_displays_all_event_items(self):
-        list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
 
-        response = self.client.get('/events/list1/')
+        response = self.client.get(f'/events/{correct_list.id}/')
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'другой элемент 1 списка')
+        self.assertNotContains(response, 'другой элемент 2 списка')
 
 
 class NewListTest(TestCase):
@@ -63,4 +66,39 @@ class NewListTest(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post('/events/new', data={'item_text': 'Новое событие'})
-        self.assertRedirects(response, '/events/list1/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, f'/events/{new_list.id}/')
+
+
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        coorrect_list = List.objects.create()
+
+        self.client.post(
+            f'/events/{coorrect_list.id}/add_item',
+            data={'item_text': 'Новый элемент существующего списка'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'Новый элемент существующего списка')
+        self.assertEqual(new_item.list, coorrect_list)
+
+    def test_redirect_to_list_view(self):
+        other_list = List.objects.create()
+        coorrect_list = List.objects.create()
+
+        response = self.client.post(
+            f'/events/{coorrect_list.id}/add_item',
+            data={'item_text': 'Новый элемент существующего списка'}
+        )
+        
+        self.assertRedirects(response, f'/events/{coorrect_list.id}/')
+
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        coorrect_list = List.objects.create()
+        response = self.client.get(f'/events/{coorrect_list.id}/')
+        self.assertEqual(response.context['list'], coorrect_list)
--- organizer/urls.py
+++ organizer/urls.py
@@ -4,5 +4,6 @@ from events import views
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('events/new', views.new_list, name='new_list'),
-    path('events/list1/', views.view_list, name='view_list'),
+    path('events/<str:list_id>/', views.view_list, name='view_list'),
+    path('events/<str:list_id>/add_item', views.add_item, name='add_item'),
 ]
