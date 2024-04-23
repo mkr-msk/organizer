0ab60dec49bac65e96207757430b1d8f03ce7639
Makarov Andrey Vladimirovich <i@makarov-podolsk.ru>
2024-04-23 15:58:03 +0300

    Использован Bootstrap для улучшения макета

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
 from selenium.common.exceptions import WebDriverException
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
 
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -103,13 +103,13 @@ class NewVisitorTest(LiveServerTestCase):
     def test_layout_and_styling(self):
 
         self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
+        self.browser.set_window_size(1920, 1080)
 
         input_boxes = self.browser.find_elements(by='tag name', value='input')
         inputbox = [ib for ib in input_boxes if ib.get_attribute('id') == 'id_new_item'][0]
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
+            630,
             delta=10
         )
 
@@ -121,6 +121,6 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = [ib for ib in input_boxes if ib.get_attribute('id') == 'id_new_item'][0]
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
+            630,
             delta=10
         )
